7eb8d97012300cfc02df945a76633de7c097118c
Fredy <tex.97@hotmail.com>
2022-12-28 14:04:13 -0500

    cambio de APP_URL

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://tkambio-api-production.up.railway.app/'),
+    'url' => env('APP_URL', 'https://tkambio-api-production.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
